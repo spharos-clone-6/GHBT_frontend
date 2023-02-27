b96f22b9ea71908ecb2e996cbe55454124b9b36c
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-02-27 14:08:40 +0900

    Init

---
+++ myStarbucks/types/types.ts
@@ -0,0 +1,12 @@
+export interface user {
+  id: number;
+  name: string;
+  email: string;
+  address?: string;
+}
+
+export interface recommandTitle {
+  id: number;
+  title: string;
+  data: object;
+}
