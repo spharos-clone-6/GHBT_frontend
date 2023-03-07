ac3586bd3ed1058e9685e49f3114cd4a700a46da
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-03-08 00:02:07 +0900

    Update [category].jsx

--- myStarbucks/pages/best/[category].jsx
+++ myStarbucks/pages/best/[category].jsx
@@ -5,18 +5,25 @@ import ProductContainer from '../../src/components/ProductContainer'
 import Axios from 'axios';
 
 export default function category() {
-  // const [itemList, setItemList] = useState([]);
-  // const API_URL = "http://backend.grapefruit-honey-black-tea.shop:5000/api/product/search/cake";
+  const [itemList, setItemList] = useState([]);
+  const API_URL = "http://backend.grapefruit-honey-black-tea.shop:5000/api/product/search/cake";
 
-  // // useEffect(() => {
-  // //   Axios.get(API_URL)
-  // //   .then((res) => {
-  // //     console.log(res);
-  // //     setItemList(res);
-  // //   });
-  // // }, [])
+  const instance = Axios.create({
+    baseURL: 'http://backend.grapefruit-honey-black-tea.shop:5000',
+    timeout: 1000,
+    withCredentials: true,
+  });
+
+
+  // useEffect(() => {
+  //   Axios.get(API_URL)
+  //   .then((res) => {
+  //     console.log(res);
+  //     setItemList(res);
+  //   });
+  // }, [])
   
-  // console.log(axiosApiInstance);
+  console.log(instance.data);
 
   return (
     <div className="container">
