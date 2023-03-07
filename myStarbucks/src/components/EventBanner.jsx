35fd2cdb1f277d9f1311889d09de66d68ef75c07
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-07 10:43:19 +0900

    Feat: 이벤트 배너 영역 컴포넌트
    이벤트 배너 영역 생성(기능 없이 이미지로 공간만 생성)

---
+++ myStarbucks/src/components/EventBanner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function EventBanner() {
+  return (
+    <section id="event-banner" className="first-section">
+      <div className="event-banner">
+        <div className="event-banner__item">
+          <div className="event-banner__item__img">
+            <img
+              src="/images/banner/banner01.png"
+              width="100%"
+              height="100%"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
