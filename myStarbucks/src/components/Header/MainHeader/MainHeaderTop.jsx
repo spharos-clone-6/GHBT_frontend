5e38776a7bbec25ef607741e394362131f08af11
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-07 09:44:25 +0900

    Feat: 메인 헤더 탑 아이콘 정상화
    아이콘 이미지 불러오기, 링크 연결

--- myStarbucks/src/components/Header/MainHeader/MainHeaderTop.jsx
+++ myStarbucks/src/components/Header/MainHeader/MainHeaderTop.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function MainHeaderTop() {
+  const router = useRouter()
   return (
     <div className="header-top">
       <div className="menu-icon">
-        <img src="assets/images/icons/menu.svg" alt="" />
+        {
+          (router.asPath === "/"
+          || router.asPath === "best" ) && (
+            <img src="/images/icons/menu.svg" alt="" />
+          )
+        }
+        {
+          router.asPath === "/product" && (
+            <img src="/images/icons/left.png" alt="" />
+          )
+        }
+        
       </div>
       <h1>온라인 스토어</h1>
       <nav>
         <ul>
           <li>
-            <img src="assets/images/icons/search.svg" />
+            <Link href="/search/search"><img src="/images/icons/search.svg" /></Link>
+            
           </li>
           <li>
-            <img src="assets/images/icons/shopping-cart.svg" />
+            <Link href="/cart/cart"><img src="/images/icons/shopping-cart.svg" /></Link>
           </li>
           <li>
-            <img src="assets/images/icons/close.png" />
+            <img src="/images/icons/close.png" />
           </li>
         </ul>
       </nav>
