03029517352e534987d4c3e670f1936fb8fcc11d
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-07 16:13:33 +0900

    Feat: 메인헤더 링크
    메뉴 누르면 이동하게 처리. active시 css 처리

--- myStarbucks/src/components/MainHeader/MainHeaderBottom.jsx
+++ myStarbucks/src/components/MainHeader/MainHeaderBottom.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function MainHeaderBottom() {
+  const router = useRouter()
   return (
     <div className="header-bottom">
       <nav>
         <ul>
-          <li className="active">
-            <a href="index.html">메인</a>
+          <li className = {router.asPath === '/' ? 'active' : ''}>
+            <Link href="/" legacyBehavior>
+              <a>메인</a>
+            </Link>
+            
           </li>
-          <li>
+          <li className={router.asPath.split('/')[1] === 'event' ? 'active' : ''}>
             <a href="event.html">기획전</a>
           </li>
-          <li>
-            <a href="best_cake.html">베스트</a>
+          <li className={router.asPath.split('/')[1] === 'best' ? 'active' : ''}>
+            <Link href="/best/cake" legacyBehavior>
+              <a>베스트</a>
+            </Link>
           </li>
-          <li>
+          <li className={router.asPath.split('/')[1] === 'mypage' ? 'active' : ''}>
             <a href="">마이페이지</a>
           </li>
         </ul>
