efcb6ba9f725b739f2d0e68e932ae2357779cb9f
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-08 10:22:14 +0900

    Feat: axios 테스트
    axios 이용으로 db연결 테스트

--- myStarbucks/pages/best/[category].jsx
+++ myStarbucks/pages/best/[category].jsx
@@ -5,25 +5,38 @@ import ProductContainer from '../../src/components/ProductContainer'
 import Axios from 'axios';
 
 export default function category() {
-  const [itemList, setItemList] = useState([]);
-  const API_URL = "http://backend.grapefruit-honey-black-tea.shop:5000/api/product/search/cake";
+  // const [itemList, setItemList] = useState([]);
+  // // const API_URL = "http://backend.grapefruit-honey-black-tea.shop:5000/api/product/search/cake";
 
-  const instance = Axios.create({
-    baseURL: 'http://backend.grapefruit-honey-black-tea.shop:5000',
-    timeout: 1000,
-    withCredentials: true,
-  });
+  // const instance = Axios.create({
+  //   baseURL: '/api/category',
+  //   timeout: 1000,
+  //   withCredentials: true,
+  // });
 
 
   // useEffect(() => {
-  //   Axios.get(API_URL)
+  //   Axios.get(`/api/product`)
   //   .then((res) => {
   //     console.log(res);
   //     setItemList(res);
   //   });
   // }, [])
   
-  console.log(instance.data);
+  // console.log(instance.data);
+
+  // const baseUrl = "http://backend.grapefruit-honey-black-tea.shop:5000";
+  
+  // const [productItem,setProcudtItem] = useState();
+
+  // useEffect(()=>{
+      
+  //     Axios(`${baseUrl}/api/category`).then((result)=>{
+  //         console.log(result.data);
+  //         setProcudtItem(result.data);
+  //     })
+  // },[])
+
 
   return (
     <div className="container">
