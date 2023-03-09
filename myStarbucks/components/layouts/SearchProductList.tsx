ca43ecc1e016ffea9334b18ae9c750071ef68dc3
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-09 09:37:24 +0900

    Fix: import 경로 해결
    경로 재설정

--- myStarbucks/components/layouts/SearchProductList.tsx
+++ myStarbucks/components/layouts/SearchProductList.tsx
@@ -1,18 +1,20 @@
-import { useParams } from "react-router";
+// import { useParams } from "react-router";
 
 function Search() {
   // 검색 버튼 눌렀을 때 나타나는 상품 목록, 복붙한 코드라 수정 필요함
-  const [searchData, setSearchData] = useState([]);
-  const params = useParams();
+  // const [searchData, setSearchData] = useState([]);
+  // const params = useParams();
 
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get(
-        "http://localhost:3000/product/search?word=" + params.word
-      );
-      console.log(result.data.result);
-      setSearchData(result.data.result);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const result = await axios.get(
+  //       "http://localhost:3000/product/search?word=" + params.word
+  //     );
+  //     console.log(result.data.result);
+  //     setSearchData(result.data.result);
+  //   }
+  //   fetchData();
+  // }, []);
 }
+
+export default Search();
