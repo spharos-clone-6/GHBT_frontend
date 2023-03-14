6cce8055aa954d0dca0ca951c1780543aabe664d
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-14 11:51:23 +0900

    Design: 마이페이지 아이콘 변경
    마이페이지 메뉴 아이콘 수정

--- myStarbucks/pages/mypage.tsx
+++ myStarbucks/pages/mypage.tsx
@@ -10,27 +10,27 @@ export default function mypage() {
           <div id="service">
             <h2>서비스</h2>
             <RightArrowMenu
-              iconSrc='/images/icons/service/shopping-list.png'
+              iconSrc='/images/icons/check-list.png'
               menuName='주문 내역'
               link=''
             />
             <RightArrowMenu
-              iconSrc='/images/icons/service/present-box.png'
+              iconSrc='/images/icons/gift-box.png'
               menuName='선물함'
               link=''
             />
             <RightArrowMenu
-              iconSrc='/images/icons/service/voucher.png'
+              iconSrc='/images/icons/discount.png'
               menuName='쿠폰'
               link=''
             />
             <RightArrowMenu
-              iconSrc='/images/icons/service/delivery-truck.png'
+              iconSrc='/images/icons/delivery-truck.png'
               menuName='배송지 관리'
               link=''
             />
             <RightArrowMenu
-              iconSrc='/images/icons/service/bell.png'
+              iconSrc='/images/icons/bell.svg'
               menuName='입고 알림 내역'
               link=''
             />
@@ -38,7 +38,7 @@ export default function mypage() {
           <div id="policy">
             <h2>약관 및 정책</h2>
             <RightArrowMenu
-              iconSrc='/images/icons/service/megaphone.png'
+              iconSrc='/images/icons/megaphone.svg'
               menuName='배송지 정보 수집 및 이용 동의'
               link=''
             />
