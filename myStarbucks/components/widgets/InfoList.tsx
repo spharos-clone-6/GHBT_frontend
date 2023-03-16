e7edecf9be7ab6b138aca106735b4c5bb169b180
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-16 16:47:44 +0900

    Style: css 코드 정리
    중복되는 css 삭제, 컴포넌트에 emotion으로 정리

--- myStarbucks/components/widgets/InfoList.tsx
+++ myStarbucks/components/widgets/InfoList.tsx
@@ -1,33 +1,41 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import React from 'react'
 import RightArrowMenu from '../ui/RightArrowMenu'
 
 export default function InfoList() {
+  const hr = css`
+    opacity: 0.5;
+    margin: 0;
+  `
+
   return (
-    <div className='info-list'>
+    <div css={css`
+      padding-left: 20px;
+      margin-bottom: 70px;
+    `}>
       <RightArrowMenu
-        iconSrc=""
         menuName="이용조건 및 배송안내"
         link=""
+        fontType='strong'
       />
-      <hr />
+      <hr css={hr}/>
       <RightArrowMenu
-        iconSrc=""
         menuName="상품제공정보고시"
         link=""
+        fontType='strong'
       />
-      <hr />
-
+      <hr css={hr}/>
       <RightArrowMenu
-        iconSrc=""
         menuName="교환/반품 안내"
         link=""
+        fontType='strong'
       />
-      <hr />
-
+      <hr css={hr}/>
       <RightArrowMenu
-        iconSrc=""
         menuName="취소/환불 안내"
         link=""
+        fontType='strong'
       />
     </div>
 
