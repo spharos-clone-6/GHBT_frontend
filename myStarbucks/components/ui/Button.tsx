9ac79c84221141941a321d1eac4fbf978d6915cf
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-03-20 16:18:17 +0900

    Feat: 버튼 컴포넌트 사용성 확대
    타입 별 여러 모양 버튼 생성 가능하도록 동적 css 적용

--- myStarbucks/components/ui/Button.tsx
+++ myStarbucks/components/ui/Button.tsx
@@ -4,8 +4,24 @@ import {css} from '@emotion/react'
 
 import { ReactNode } from "react";
 
-export default function Button(props: { children: ReactNode, btnType: "button" | "submit" | "reset" | undefined, btnEvent: React.MouseEventHandler<HTMLButtonElement> | undefined }) {
-  const { btnType, children, btnEvent } = props
+export default function Button(props: 
+  { children: ReactNode, btnType: "button" | "submit" | "reset" | undefined, 
+  type?: string, btnEvent: React.MouseEventHandler<HTMLButtonElement> | undefined }) {
+  const { btnType, children, type, btnEvent } = props
+
+  let backgroundColor = 'var(--color-light-green)'
+  let fontColor = 'var(--color-white)'
+  let border = 'none'
+
+  if(type === 'disabled') {
+    backgroundColor = 'var(--color-gray-button)'
+    fontColor = 'var(--color-white)'
+  } else if(type === 'white') {
+    backgroundColor = 'var(--color-white)'
+    fontColor = 'var(--color-light-green)'
+    border = '1px solid var(--color-light-green)'
+  }
+
   const style = css`
     border-radius: 30px;
     width: 90%;
@@ -13,6 +29,9 @@ export default function Button(props: { children: ReactNode, btnType: "button" |
     background-color: var(--color-light-green);
     border: none;
     color: var(--color-white);
+    background-color: ${backgroundColor};
+    border: ${border};
+    color: ${fontColor};
     margin: 15px 0;
     letter-spacing: -0.2px;
   `
