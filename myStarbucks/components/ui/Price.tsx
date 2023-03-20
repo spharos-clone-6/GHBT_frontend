1bf68f5296520ec58749d18de7148a16f9284e93
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-03-20 23:59:50 +0900

    Design: 가격 폰트 사이즈 emotion 적용
    폰트 사이즈를 상위 컴포넌트에서 정할 수 있도록 emotion 적용

--- myStarbucks/components/ui/Price.tsx
+++ myStarbucks/components/ui/Price.tsx
@@ -1,13 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import React from 'react'
 
 interface Props {
   price?: number
+  size?: string
 }
 
-export default function Price({price = 0}: Props) {
+export default function Price({price = 0, size = 'inherit'}: Props) {
+  const style = css`
+    font-size: ${size};
+  `
+
   return (
     <>
-      <span>{price.toLocaleString('ko-KR')}</span>원
+      <span css={style}>{price.toLocaleString('ko-KR')}</span>원
     </>
   )
 }
