f8c77d5ab7c2326411d358081ae392009162d879
DESKTOP-0AJNRUV\교육생20 <qwer15948@naver.com>
2023-03-21 19:31:05 +0900

    Feat: 목차 모달 처리(진행중)

---
+++ myStarbucks/components/modals/Contents.tsx
@@ -0,0 +1,32 @@
+import RightArrowMenu from "@/components/ui/RightArrowMenu";
+import ContentCategoryContainer from "@/components/widgets/ContentCategoryContainer";
+import React from "react";
+
+export default function contents() {
+  return (
+    <div>
+      <section className="contents-head">
+        {/* 닫기 */}
+        <div className="close-icon">
+          <img src="assets/images/icons/menu.svg" alt="" />
+        </div>
+        {/*본문*/}
+        <div className="contents-msg">
+          <div className="msg-title">Sign in to Online Store</div>
+          <div>
+            <a href="">로그인</a> 후 이용해 보세요.
+          </div>
+        </div>
+        <hr className="contents-line" />
+      </section>
+      {/* 제품 카테고리 */}
+      <ContentCategoryContainer />
+      {/*기획전/베스트 이동*/}
+      <section id="nav-event-best">
+        <RightArrowMenu menuName={"기획전"} link={"/event?category=케이크"} fontType='strong' description="진행중인 기획전을 만나보세요."/>
+        <hr style={{margin: '15px 0'}}/>
+        <RightArrowMenu menuName={"베스트"} link={"/best?category=케이크"} fontType='strong' description="스타벅스의 베스트 상품을 만나보세요."/>
+      </section>
+    </div>
+  );
+}
