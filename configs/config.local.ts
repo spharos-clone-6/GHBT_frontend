1d7230b79b47f8a9ce8c25132ea60b8379fd90f2
DESKTOP-HTDC06N\교육생19 <hootingowl09@naver.com>
2023-03-24 15:31:12 +0900

    Fix: 백엔드 서버 base url 수정

--- configs/config.local.ts
+++ configs/config.local.ts
@@ -2,7 +2,7 @@
 import getConfigs from "./config.common";
 
 // 환경마다 달라져야 할 변수, 값들을 정의합니다. (여기는 local 환경에 맞는 값을 지정합니다.)
-const baseUrl = 'https://localhost:8080/';
+const baseUrl = 'http://backend.grapefruit-honey-black-tea.shop';
 const mode = 'local';
 
 // 환경마다 달라져야 할 값들을 getConfig 함수에 전달합니다.
