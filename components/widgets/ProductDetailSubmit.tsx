073750a6823fb42d86090cef283c409b97eb6c4d
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-03-24 16:03:38 +0900

    Fix: 상세페이지 가격 오류 수정
    수량 출력 컴포넌트 프롭스 변경으로인한 이슈 수정

--- components/widgets/ProductDetailSubmit.tsx
+++ components/widgets/ProductDetailSubmit.tsx
@@ -7,21 +7,34 @@ import Button from "../ui/Button";
 import ItemAmount from "../ui/ItemAmount";
 import Price from "../ui/Price";
 
-export default function ProductDetailSubmit(props: { price: number, productName: string, handleIsOpen: React.MouseEventHandler<HTMLDivElement>}) {
-  const { price, productName, handleIsOpen } = props
+export default function ProductDetailSubmit(props: {
+  price: number;
+  productName: string;
+  handleIsOpen: React.MouseEventHandler<HTMLDivElement>;
+}) {
+  const { price, productName, handleIsOpen } = props;
   const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [itemCount, setItemCount] = useState<number>(1);
 
   const totalPriceStyle = css`
     text-align: right;
     padding: 0 30px;
     margin-bottom: 80px;
     font-weight: bold;
-  `
+  `;
 
   return (
     <div onClick={handleIsOpen}>
-      <ItemAmount price={price} label={productName} setTotalPrice={setTotalPrice}/>
-      <p css={totalPriceStyle}>합계 <Price price={totalPrice} size='large'/></p>
+      <ItemAmount
+        price={price}
+        label={productName}
+        setTotalPrice={setTotalPrice}
+        count={itemCount}
+        setCount={setItemCount}
+      />
+      <p css={totalPriceStyle}>
+        합계 <Price price={totalPrice} size="large" />
+      </p>
     </div>
   );
 }
