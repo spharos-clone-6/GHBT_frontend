6efb2bc0db328e47cc2474a839331ad7618d5f44
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-03-27 10:28:04 +0900

    Fix: 디테일 이미지 수정
    디테일 이미지 이상하게 적용됐던 부분 원래대로 되돌림

--- components/ui/DetailImage.tsx
+++ components/ui/DetailImage.tsx
@@ -3,14 +3,11 @@ import Image from "next/image";
 
 function DetailImage(props: { url: string }) {
   return (
-    // <div style={{ width: "100%", position: "relative", height: "100%" }}>
-    <Image
+    <img
       src={`https://storage.googleapis.com/ghbt/product_image/${props.url}`}
-      alt="상품상세이미지"
-      fill
-      sizes="100vw"
+      alt="상세이미지"
+      width={"100%"}
     />
-    // </div>
   );
 }
 
