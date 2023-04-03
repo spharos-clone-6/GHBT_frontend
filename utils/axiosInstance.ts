2a1efef34317d2ee5554c63956c63c5c843fb236
nam <namgh627@naver.com>
2023-04-03 17:22:31 +0900

    Fix : devops

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -15,8 +15,26 @@ const AxiosInterceptor = ({ children }: any) => {
 
   axiosApiInstance.interceptors.response.use(
     (response) => response,
-    (error) => {
-      console.log("ggg", error);
+    async (error) => {
+      if (error.response.status === 401) {
+        try {
+          const originalRequest = error.config;
+          console.log("origin에러", originalRequest);
+          const data = await axiosApiInstance.post("reissue");
+
+          if (data?.headers?.authorization) {
+            const accessTokenByRefresh = data?.headers?.authorization;
+            const setAcessTokenByRefresh = accessTokenByRefresh.replace(
+              /Bearer /g,
+              ""
+            );
+            setAccessToken(setAcessTokenByRefresh);
+            console.log("엑세스토큰", accessTokenByRefresh);
+            originalRequest.headers.Authorization = `Bearer ${setAcessTokenByRefresh}`;
+            return await axiosApiInstance.request(originalRequest);
+          }
+        } catch (error) {}
+      }
     }
   );
 
