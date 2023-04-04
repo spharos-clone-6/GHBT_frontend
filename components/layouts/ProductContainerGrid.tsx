acfa45a1c3ed126f441183adbb55be7ee4f6872a
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-04-04 16:42:12 +0900

    Fix: 아이템 컨테이너 스타일 속성 수정

--- components/layouts/ProductContainerGrid.tsx
+++ components/layouts/ProductContainerGrid.tsx
@@ -2,18 +2,23 @@ import { productType } from "@/types/types";
 import { useEffect } from "react";
 import NoItem from "../ui/NoItem";
 import ProductItemCol from "../ui/ProductItemCol";
+import { useRouter } from "next/router";
 
 type Item = {
   itemList: productType[];
 };
 
 export default function ProductContainerGrid({ itemList = [] }: Item) {
+  const { pathname } = useRouter();
   return (
     <>
       {itemList.length === 0 ? (
         <NoItem />
       ) : (
-        <div className="product-container" style={{ minHeight: "95vh" }}>
+        <div
+          className="product-container"
+          style={pathname === "/store" ? { minHeight: "95vh" } : {}}
+        >
           {itemList &&
             itemList.map((item: productType, idx: number) => (
               <ProductItemCol key={idx} item={item} idx={idx} />
