a89a3d147923ccf7beee8adaff503e208eaf3f00
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-04-04 10:41:49 +0900

    Design: 로그인 css 모바일 버전 수정

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+
 import BottomFixedContainer from "@/components/ui/BottomFixedContainer";
 import Button from "@/components/ui/Button";
 import Config from "@/configs/config.export";
@@ -6,9 +8,9 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { useRecoilState } from "recoil";
-import Image from "next/image";
 import BackIcon from "@/components/ui/BackIcon";
 import Link from "next/link";
+import { css } from "@emotion/react";
 
 export default function Login() {
   const { baseUrl } = Config();
@@ -49,24 +51,14 @@ export default function Login() {
     setPassword(event.target.value);
   };
 
-  // const modalStyle: Object = {
-  //   position: "fixed",
-  //   backgroundColor: "white",
-  //   top: "0",
-  //   left: "0",
-  //   zIndex: 999,
-  //   width: "100%",
-  //   height: "100%",
-  // };
-
   return (
     <div>
       <BackIcon />
       <div>
-        <section className="login-header">
+        <section className="login-header" css={loginHeader}>
           <h1>로그인</h1>
         </section>
-        <section className="greeting">
+        <section className="greeting" css={greeting}>
           <img id="starbucks-logo" src="./images/starbucks-logo.png" />
           <h2>
             안녕하세요.
@@ -98,3 +90,48 @@ export default function Login() {
     </div>
   );
 }
+
+const loginHeader = css`
+  padding: 0 15px;
+  margin-top: 40px;
+  & h1 {
+    font-size: 2rem;
+    margin: 0;
+    padding: 0;
+  }
+
+  @media screen and (max-height: 700px) {
+    margin-top: 20px;
+    & h1 {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+const greeting = css`
+  margin-top: 30px;
+  padding: 20px;
+  & h2 {
+    font-size: 25px;
+    font-weight: bold;
+    letter-spacing: -1px;
+    line-height: 30px;
+    margin-bottom: 0px;
+  }
+  & p {
+    font-size: 0.9rem;
+  }
+
+  @media screen and (max-height: 700px) {
+    margin-top: 20px;
+    margin-bottom: 0;
+    padding-bottom: 0;
+    & h2 {
+      font-size: 1.2rem;
+      line-height: 25px;
+    }
+    & p {
+      font-size: 0.8rem;
+    }
+  }
+`;
