a3c7c7b57948a44737f5beffabe9df6ce1e48242
nam <namgh627@naver.com>
2023-04-04 10:11:59 +0900

    Fix : devops url

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -66,7 +66,7 @@ export default function Cart() {
         console.log("일반 상품 :", generalResult);
         if (generalResult) setGeneralCart(generalResult?.data);
         else setGeneralCart([]);
-      }, 100);
+      }, 200);
     }
   }
   async function fetchFrozenData() {
